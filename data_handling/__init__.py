a2c685095f5bd4941996fe4af41625551ee620fa
rkp8000 <rpang.contact@gmail.com>
2015-04-20 10:35:01 -0700

    Add ability to return left minus right zscore and bar position in degrees centered around 0

--- data_handling/__init__.py
+++ data_handling/__init__.py
@@ -2,12 +2,16 @@ __all__ = []
 
 import datetime
 import numpy as np
+from scipy.stats import zscore
 
 # default header block size
 HEADER_BLOCK_SIZE = 2048
+# conversion factor from barpos voltage to degree
+BARPOS_CONVERSION = 360/5.
 
 
-def load_edr(fname, header_block_size=HEADER_BLOCK_SIZE, dt=.001):
+def load_edr(fname, header_block_size=HEADER_BLOCK_SIZE, dt=.001,
+             lmr_zcore=True, barpos_in_degrees=True):
     """Load header info and data from binary file."""
 
     if fname[-4:].lower() != '.edr':
@@ -40,7 +44,7 @@ def load_edr(fname, header_block_size=HEADER_BLOCK_SIZE, dt=.001):
                 end_of_header = True
 
         # get column names
-        col_names = [header['YN%d'%ii] for ii in range(header['NC'])]
+        cols = [header['YN%d'%ii] for ii in range(header['NC'])]
 
         # get normalization constants from header
         ad = float(header['AD'])
@@ -50,22 +54,22 @@ def load_edr(fname, header_block_size=HEADER_BLOCK_SIZE, dt=.001):
         f.seek(header_block_size)
 
         # load data into array
-        num_cols = header['NC']
-        num_rows = header['NP']/num_cols
-        data = np.fromfile(f, dtype=np.int16).reshape((num_rows, num_cols))
+        ncols = header['NC']
+        nrows = header['NP']/ncols
+        data = np.fromfile(f, dtype=np.int16).reshape((nrows, ncols))
         data = data.astype(float)
         # normalize data by calibration, etc.
-        for channel in range(num_cols):
+        for channel in range(ncols):
             ych_channel = 'YCF%d' % channel
             data[:, channel] *= (float(ad) / (adcmax * header[ych_channel]))
 
         # add time as first column
         time = np.arange(data.shape[0])[:, None] * header['DT']
         data = np.concatenate([time, data], axis=1)
-        col_names = ['time'] + col_names
+        cols = ['time'] + cols
 
         # swap frequency & LmR columns
-        col_names[3], col_names[4] = col_names[4], col_names[3]
+        cols[3], cols[4] = cols[4], cols[3]
         data[:, [3, 4]] = data[:, [4, 3]]
 
         # downsample data
@@ -77,9 +81,19 @@ def load_edr(fname, header_block_size=HEADER_BLOCK_SIZE, dt=.001):
                 idxs = idxs[:-1]
             data = data[idxs, :]
 
+    if lmr_zcore:
+        lampz = zscore(data[:, cols.index('Lamp')])
+        rampz = zscore(data[:, cols.index('Ramp')])
+        data[:, cols.index('LmR')] = lampz - rampz
+
+    if barpos_in_degrees:
+        barpos = data[:, cols.index('Barpos')] * BARPOS_CONVERSION
+        barpos[barpos > 180] -= 360
+        data[:, cols.index('Barpos')] = barpos
+
     # create datetime object for file start time
     dt_format = '%m-%d-%Y %I:%M:%S %p'
     file_start_str = header['CTIME']
     file_start = datetime.datetime.strptime(file_start_str, dt_format)
 
-    return data, file_start, col_names, header
+    return data, file_start, cols, header
